9b740f913276abb1dd87569644506990400e72fa
Jonas Jeronimo <jonas.jeronimo@live.com>
2022-09-10 22:27:59 -0300

    requisito 8 - em andamento

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 
 export default class Cart extends Component {
+  state = {
+    items: [],
+  };
+
   componentDidMount() {
     const xablau = JSON.parse(localStorage.getItem('produto'));
-    console.log(xablau);
+    this.setState({ items: xablau });
   }
 
   render() {
+    const { items } = this.state;
     return (
-      <div data-testid="shopping-cart-empty-message">
-        Seu carrinho está vazio
-      </div>
+      <>
+        <div data-testid="shopping-cart-empty-message">
+          Seu carrinho está vazio
+        </div>
+        <div>
+          { items.map((el, i) => (
+            <div key={ `${el.title}-${i}` }>
+              <p data-testid="shopping-cart-product-name">{ el.title }</p>
+              <p>{ el.price }</p>
+              <p data-testid="shopping-cart-product-quantity">{ el. }</p>
+            </div>
+          )) }
+        </div>
+      </>
     );
   }
 }
-
-// oi
